ee2d3b5e69bc9eba3dde8052d965ec084b9a95e5
Grant Sanderson <grant@3blue1brown.com>
2024-02-04 14:45:52 -0600

    Update sync_updated_translation.py to take in description and title files

--- scripts/sync_updated_translation.py
+++ scripts/sync_updated_translation.py
@@ -9,6 +9,7 @@ from translate import sentence_translations_to_srt
 
 from upload import get_youtube_api
 from upload import upload_caption
+from upload import upload_video_localizations
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Video url')
@@ -19,10 +20,13 @@ if __name__ == "__main__":
 
     os.sep.split()
 
+    youtube_api = get_youtube_api()
     cap_dir = get_web_id_to_caption_directory_map()[web_id]
-    trans_file = Path(cap_dir, language, file_name)
     video_id = get_web_id_to_video_id_map()[web_id]
 
-    youtube_api = get_youtube_api()
-    new_srt = sentence_translations_to_srt(trans_file)
-    upload_caption(youtube_api, video_id, new_srt, replace=True)
+    if file_name == "sentence_translations.json":
+        trans_file = Path(cap_dir, language, file_name)
+        new_srt = sentence_translations_to_srt(trans_file)
+        upload_caption(youtube_api, video_id, new_srt, replace=True)
+    elif file_name in ["title.json", "description.json"]:
+        upload_video_localizations(youtube_api, cap_dir, video_id)
