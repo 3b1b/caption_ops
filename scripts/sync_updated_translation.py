da0548de54a9ca5c758c9d87c973f86b35983eca
Grant Sanderson <grant@3blue1brown.com>
2024-02-07 15:33:32 -0600

    Allow for passing multiple files into sync_updated_translations

--- scripts/sync_updated_translation.py
+++ scripts/sync_updated_translation.py
@@ -11,12 +11,9 @@ from upload import get_youtube_api
 from upload import upload_caption
 from upload import upload_video_localizations
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Video url')
-    parser.add_argument('translation_file_path_tail', type=str, help='Translation file path tail')
-    args = parser.parse_args()
 
-    web_id, language, file_name = args.translation_file_path_tail.split(os.sep)[-3:]
+def sync_json(json_file_path_tail):
+    web_id, language, file_name = json_file_path_tail.split(os.sep)[-3:]
 
     os.sep.split()
 
@@ -30,3 +27,19 @@ if __name__ == "__main__":
         upload_caption(youtube_api, video_id, new_srt, replace=True)
     elif file_name in ["title.json", "description.json"]:
         upload_video_localizations(youtube_api, cap_dir, video_id)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Video url')
+    parser.add_argument('file', type=str, help='Translation file path (or tail of the path)')
+    args = parser.parse_args()
+
+    if args.file.endswith(".json"):
+        sync_json(args.file)
+    elif args.file.endswith(".txt"):
+        lines = Path(args.file).read_text().split("\n")
+        for line in lines:
+            try:
+                sync_json(line)
+            except Exception as e:
+                print(f"Failed to sync {line}\n{e}\n")
